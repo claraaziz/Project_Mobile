c82fab9d45a3586ac2fb21af7d7f11f3a870d851
claraaziz <cocca.az@gmail.com>
2022-03-30 00:28:43 +0200

    home

--- app/src/main/java/com/example/projectmobile/HomeActivity.java
+++ app/src/main/java/com/example/projectmobile/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.projectmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void launchClassesActivity(View view) {
+        Intent classesIntent = new Intent(this, Classes.class);
+        startActivity(classesIntent);
+    }
+
+    public void launchWorkoutActivity(View view) {
+        Intent workoutIntent = new Intent(this, Workout.class);
+        startActivity(workoutIntent);
     }
 }
