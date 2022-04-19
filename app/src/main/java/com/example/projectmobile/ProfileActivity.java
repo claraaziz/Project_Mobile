b94cb9b81f1b4c4991a69fde9323b2f5ab9758f3
RaniaEldahrani <sim.raniaahmed3045@alexu.edu.eg>
2022-04-20 01:20:09 +0200

    .

--- app/src/main/java/com/example/projectmobile/ProfileActivity.java
+++ app/src/main/java/com/example/projectmobile/ProfileActivity.java
@@ -2,13 +2,30 @@ package com.example.projectmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    TextView n,a,w,h;
+    String name,age,weight,height;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        n=findViewById(R.id.n);
+        a=findViewById(R.id.a);
+        h=findViewById(R.id.h);
+        w=findViewById(R.id.w);
+        Intent i = getIntent();
+        name=i.getStringExtra(RegisterActivity.EXTRA_MESSAGE1);
+        age=i.getStringExtra(RegisterActivity.EXTRA_MESSAGE2);
+        height=i.getStringExtra(RegisterActivity.EXTRA_MESSAGE3);
+        weight=i.getStringExtra(RegisterActivity.EXTRA_MESSAGE4);
+        n.setText(name);
+        a.setText(age+" years");
+        h.setText(height+"cm");
+        w.setText(weight+"kg");
     }
 }
--- app/src/main/java/com/example/projectmobile/RegisterActivity.java
+++ app/src/main/java/com/example/projectmobile/RegisterActivity.java
@@ -2,16 +2,28 @@ package com.example.projectmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText name,age,height,weight;
-    private RadioGroup gender;
+    public static final String LOG_TAG ="Key";
+    public static final String EXTRA_MESSAGE1=
+            "com.example.android.myapplication3.extra.MESSAGE";
+    public static final String EXTRA_MESSAGE2=
+            "com.example.android.myapplication3.extra.MESSAGE";
+    public static final String EXTRA_MESSAGE3=
+            "com.example.android.myapplication3.extra.MESSAGE";
+    public static final String EXTRA_MESSAGE4=
+            "com.example.android.myapplication3.extra.MESSAGE";
+    EditText name,age,height,weight;
+    RadioGroup gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +34,19 @@ public class RegisterActivity extends AppCompatActivity {
         weight=findViewById(R.id.weight);
         gender=findViewById(R.id.gender);
     }
-
+    public void launchProfileActivity(View view){
+        Log.d(LOG_TAG,"Button Clicked!");
+        Intent i = new Intent(this,ProfileActivity.class);
+        String n = name.getText().toString();
+        Integer a= Integer.parseInt(age.getText().toString());
+        Integer h=Integer.parseInt(height.getText().toString());
+        Integer w=Integer.parseInt(weight.getText().toString());
+        i.putExtra(EXTRA_MESSAGE1,n);
+        i.putExtra(EXTRA_MESSAGE2,a);
+        i.putExtra(EXTRA_MESSAGE3,h);
+        i.putExtra(EXTRA_MESSAGE4,w);
+        startActivity(i);
+    }
     @Override
     protected void onPause() {
         super.onPause();
