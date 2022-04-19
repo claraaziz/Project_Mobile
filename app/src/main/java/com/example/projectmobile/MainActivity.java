231468fc348292ef9d681d274244e3163e8f3f5c
RaniaEldahrani <sim.raniaahmed3045@alexu.edu.eg>
2022-04-20 00:05:40 +0200

    .

--- app/src/main/java/com/example/projectmobile/MainActivity.java
+++ app/src/main/java/com/example/projectmobile/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.projectmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Handler;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 4000;
+    private BroadcastReceiver MyReciever=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyReciever=new MyReciever();
+        broadcastIntent();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -21,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
                 finish();}
         }, SPLASH_TIME_OUT);
+    }
+    public void broadcastIntent(){
+        registerReceiver(MyReciever,new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(MyReciever);
     }
 }
--- app/src/main/java/com/example/projectmobile/RegisterActivity.java
+++ app/src/main/java/com/example/projectmobile/RegisterActivity.java
@@ -2,13 +2,55 @@ package com.example.projectmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText name,age,height,weight;
+    private RadioGroup gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        name=findViewById(R.id.name);
+        age=findViewById(R.id.age);
+        height=findViewById(R.id.height);
+        weight=findViewById(R.id.weight);
+        gender=findViewById(R.id.gender);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sp = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit=sp.edit();
+        myEdit.putString("name",name.getText().toString());
+        myEdit.putInt("age",Integer.parseInt(age.getText().toString()));
+        myEdit.putInt("height",Integer.parseInt(height.getText().toString()));
+        myEdit.putInt("weight",Integer.parseInt(weight.getText().toString()));
+        myEdit.putInt("gender",gender.indexOfChild(findViewById(gender.getCheckedRadioButtonId())));
+        myEdit.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sh = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        String s= sh.getString("name","");
+        int a = sh.getInt("age",0);
+        int h = sh.getInt("height",0);
+        int w = sh.getInt("weight",0);
+        int i = sh.getInt("gender",-1);
+        if( i >= 0){
+            ((RadioButton) ((RadioGroup)findViewById(R.id.gender)).getChildAt(i)).setChecked(true);
+        }
+        name.setText(s);
+        age.setText(String.valueOf(a));
+        height.setText(String.valueOf(h));
+        weight.setText(String.valueOf(w));
     }
 }
